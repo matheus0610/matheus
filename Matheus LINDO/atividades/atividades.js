fc088d711d21b13c7fd49b16c430759053699713
matheus0610 <mtheu246@gmail.com>
2025-02-20 07:50:42 -0300

    Add files via upload

---
+++ Matheus LINDO/atividades/atividades.js
@@ -0,0 +1,91 @@
+console.log("")
+const nome = "matheus"
+console.log("meu nome é: "+nome)
+
+console.log("")
+
+console.log("atividade 2")
+
+console.log("")
+const num1 = 3
+const num2 = 4
+
+const soma = num1+num2
+console.log("a soma é: "+soma)
+
+console.log("")
+
+console.log("atividade 3")
+console.log("")
+const frase1 = ("bom ")
+const frase2 = ("dia")
+const junçao = frase1+frase2
+console.log(junçao)
+console.log("")
+console.log("atividade 4")
+const n1 = 6
+const n2 = 7
+const n3 = 8
+const media = (n1+n2+n3)/3
+console.log("a média dos numeros é: "+media)
+
+console.log("")
+
+console.log("atividade 5")
+
+let numero1 = 6
+let numero2 = 5
+
+console.log("numero1: "+numero1)
+console.log("numero2: "+numero2)
+
+
+let auxiliar = numero1
+numero1 = numero2
+numero2 = auxiliar
+console.log("numero1: "+numero1)
+console.log("numero2: "+numero2)
+
+console.log("")
+
+console.log("atividade 6")
+
+const largura = 5
+const altura = 10
+const area = altura*largura
+console.log("a area é: "+ area)
+
+console.log("")
+
+console.log("atividade 7")
+
+const celsius = 25
+const fahrenheit = (celsius*9/5)+32
+console.log("a temperatura em fahrenheit é: "+fahrenheit)
+
+console.log("")
+
+console.log("atividade 8")
+
+const nume1 = 5
+const nq = nume1*nume1
+console.log("o numero ao quadrado é: "+nq)
+
+console.log("")
+
+console.log("atividade 9")
+
+const anoNascimento = 2008
+const anoAtual = 2025
+const idade = anoAtual-anoNascimento
+
+console.log("a idade da pessoa é: "+idade)
+
+console.log("")
+
+console.log("atividade 10")
+
+const precoOriginal = 50
+const descontoPorcentagem = 0.10
+const desconto = precoOriginal*descontoPorcentagem
+console.log("o desconto vai ser de: $"+desconto);
---
+++ Matheus LINDO/exemplos/nomee.js
@@ -0,0 +1,6 @@
+const nome = "Matheus";
+const sobreNome ="Felipe"
+const nomeCompleto = nome + " " + sobreNome;
+
+console.log(nome,sobreNome);
+console.log(nomeCompleto);
